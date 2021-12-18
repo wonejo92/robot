1a1b552a913470e6ef6eb67b7fc1f0aad86734fa
Christian Yunga <ychristian@est.ups.edu.ec>
2021-12-18 14:03:02 -0500

    conexion con telegram

--- main.py
+++ main.py
@@ -1,59 +1,34 @@
-import DB_utility
-import procesos
+import constants as keys
+from telegram.ext import *
+import respuestas 
 
+print('Iniciando bot')
 
-conexion=DB_utility.DBConnector()
-optionMenu={}
+def start_command(update,context):
+    update.message.reply_text('Escribeme !')
 
-def initialMessage():
-    print('Hola soy CORA 😉, tu asistente virtual !! \n\n'
-          '¿ Tienes cuenta con nosotros ? \n'
-          '1 : SI \n'
-          '2 : NO \n')
-    answer= eval(input())
+def help_command(update,context):
+    update.message.reply_text('En que puedo ayudarte ?')
 
-    if(answer==1):
-        #print('Tiene cuenta')
-        print('\n')
-        result=procesos.login()
-        if(result==True):
-            showMenu()
-    else:
-        print('No tiene cuenta')
+def handle_message(update,context):
+    text = str(update.message.text).lower()
+    response = respuestas.sample_responses(text)
 
+    update.message.reply_text(response)
 
-def showMenu():
-    print('¿ Como te ayudo ? \n\n'
-          '1. Agendar una cita \n'
-          '2. Consulta de cuenta \n'
-          '3. Consulta de millas \n'
-          '4. Bloqueo de Tarjetas \n'
-          '5 Desbloqueo de tarjetas \n'
-          '6 Consultas Generales \n'
-          '7 Dejar un comentario \n')
+def error(update,context):
+    print(f'Update {update} caused error {context.error} ')
 
-    optionMenu={"1": "Agendar_Cita", "2": "Consulta_Cuenta", "3": "Consulta_Millas", "4": "Bloqueo_Tarjeta", "5":
-                "Desbloqueo_Tarjeta", "6": "Consultas_Generales", "7": "Dejar_Comentario"}
+def main():
+    updater = Updater(keys.API_KEY, use_context = True)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('start', start_command))
+    dp.add_handler(CommandHandler('help', help_command))
+    dp.add_handler(MessageHandler(Filters.text,handle_message))
+    dp.add_error_handler(error)
+    updater.start_polling()
+    updater.idle()
+    
 
-    answerMenu=input()
-    #print(optionMenu[answerMenu])
-    match optionMenu[answerMenu]:
-        case "Agendar_Cita":
-            procesos.Agendar_Cita()
-            showMenu()
-        case "Consulta_Cuenta":
-            procesos.Consulta_Cuenta()
-            showMenu()
-        case "Consulta_Millas":
-            procesos.obtenerMillas()
-            showMenu()
-        case "Bloqueo_Tarjeta":
-            procesos.bloqueoDesbloqueoTarjeta('bloquear')
-            showMenu()
-        case "Desbloqueo_Tarjeta":
-            procesos.bloqueoDesbloqueoTarjeta('desbloquear')
-            showMenu()
-        case "Consultas_Generales":
-            procesos.Consultas_Generales()
+main()
 
-initialMessage()
