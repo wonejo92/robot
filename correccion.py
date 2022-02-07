f429828886654974ad3fcdd2f75d1bb207da9f95
Kevin Cordero <ismael-3310@hotmail.com>
2022-02-07 17:37:48 -0500

    Normalizacion

--- correccion.py
+++ correccion.py
@@ -122,8 +122,20 @@ def obtenerRespuesta(mensaje):
     return respuesta
 
 
+def normalize(s):
+    replacements = (
+        ("á", "a"),
+        ("é", "e"),
+        ("í", "i"),
+        ("ó", "o"),
+        ("ú", "u"),
+    )
+    for a, b in replacements:
+        s = s.replace(a, b).replace(a.upper(), b.upper())
+    return s
 
-
+#print(normalize("¡Hólá, múndó!"))
+#print(normalize("¡HÓLÁ, MÚNDÓ!"))
             
 
     
